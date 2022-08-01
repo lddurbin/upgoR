28ae0e074623bac688c12ad79214a1ddc2628bd0
lddurbin <l.d.durbin@gmail.com>
2022-08-01 20:36:08 +1200

    Reference to data source

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,5 +1,6 @@
 ## code to prepare `DATASET` dataset goes here
 
+# https://sourceforge.net/projects/wordlist/
 word_list <- read.csv(here("data-raw/british-words.50"), header = FALSE)
 
 usethis::use_data(word_list, overwrite = TRUE, internal = TRUE)
