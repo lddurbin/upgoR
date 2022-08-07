125009354b92738698d8249b5837a3c69fe1fcc1
lddurbin <l.d.durbin@gmail.com>
2022-08-07 22:16:50 +1200

    Added roxygen comment and ran document()

--- R/text_analysis.R
+++ R/text_analysis.R
@@ -1,3 +1,12 @@
+#' Read a text file as a character vector
+#'
+#' @param file A single .txt file
+#'
+#' @return A character vector
+#' @export
+#'
+#' @examples
+#' read_txt("text.txt")
 read_txt <- function(file) {
   scan(file, character(), quote = "")
 }
